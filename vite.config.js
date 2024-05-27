f76f6b56f8ecaaadaaf9b71f53c1275e220e64a9
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-05-27 14:44:08 -0300

    Set Up Project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable no-undef */
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        includePaths: [resolve(__dirname, "src")],
+      },
+    },
+  },
+});
