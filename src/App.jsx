595673401c34e2fd166ce0429858f3bac2a73498
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-06-04 17:46:12 -0300

    Implemented Home carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import '@/App.scss'
 import CustomRouter from "@/routes.jsx";
+import { useEffect } from 'react';
+import { register } from 'swiper/element/bundle';
 
 function App() {
+
+  useEffect(() => {
+    register()
+  }, [])
+
   return (
     <div className="App">
       <CustomRouter />
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
 
 const Home = () => {
   return (
-    <div className='Home'>Home</div>
-  )
-}
+    <div className="Home">
+      <section className="carousel">
+        <swiper-container
+          slidesPerView={1}
+          autoplay={true}
+          pagination={{ clickable: true }}
+          speed={1000}
+          loop={true}
+        >
+          <swiper-slide>
+            <Link>ey1</Link>
+          </swiper-slide>
+          <swiper-slide>
+            <Link>ey2</Link>
+          </swiper-slide>
+          <swiper-slide>
+            <Link>ey3</Link>
+          </swiper-slide>
+          <swiper-slide>
+            <Link>ey4</Link>
+          </swiper-slide>      
+        </swiper-container>
+      </section>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
