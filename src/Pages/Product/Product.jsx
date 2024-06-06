02d17429c7dd560da7d560ac8ec46acb9406900c
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-06-06 15:54:22 -0300

    Implemented Product Layout

---
+++ src/Pages/Product/Product.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useParams } from "react-router-dom";
+
+const Product = () => {
+  const { id } = useParams();
+
+  return (
+    <div className='Product'>
+      Product {id}
+    </div>
+  )
+}
+
+export default Product
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,8 @@
 import { Navigate, useRoutes } from "react-router-dom";
 import Layout from "@/Layout/Layout";
+import LayoutProduct from "@/Layout/LayoutProduct";
 import Home from '@/Pages/Home/Home';
+import Product from '@/Pages/Product/Product';
 
 const CustomRouter = () => {
   return useRoutes([
@@ -18,6 +20,22 @@ const CustomRouter = () => {
       path: "*",
       element: <Navigate to={"/"} />,
     },
+    {
+      path: "/product",
+      element: <Layout />,
+      children: [
+        {
+          element: <LayoutProduct />,
+          children:[
+            {
+              path: "/product/:id",
+              index: true,
+              element: <Product />
+            }
+          ]
+        }
+      ]
+    }
   ]);
 };
 
