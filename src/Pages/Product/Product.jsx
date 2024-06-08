2a1578b2a30466de5eeb4e4c8b4681c4f4c6d08b
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-06-07 23:16:41 -0300

    Advanced Product Details

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -1,25 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "swiper/css";
+import data from "@/Utils/productList.json";
 
 const Product = () => {
   const { id } = useParams();
 
+  const [Product, setProduct] = useState(null);
+  useEffect(() => {
+    const product = data.find((x) => x.id === Number(id));
+    setProduct(product);
+  }, [id]);
+
   return (
     <div className="Product">
-      <section className="div1">div1</section>
-      <section className="div2">div2</section>
-      <section className="div3">div3</section>
-      <section className="div4">div4</section>
-      <section className="div5">div5</section>
-      <section className="div6">div6</section>
-      <section className="div7">div7</section>
-      <section className="div8">div8</section>
-      <section className="div9">div9</section>
-      <section className="div10">div10</section>
-      <section className="div11">div11</section>
-      <section className="div12">div12</section>
-      <section className="div13">div13</section>
+      <section className="div1 mainInfo">
+        <div className="portion">
+          <span className="category">{Product?.category}</span>
+          <span className="name">{Product?.name}</span>
+          <span className="provider">{Product?.provider}</span>
+          <span className="description">{Product?.description}</span>
+        </div>
+        <div className="extraInfo portion">
+          <section className="measures">
+            <span className="title">Medidas:</span>
+            <span className="data">{Product?.measures}</span>
+          </section>
+          <section className="quality_code">
+            <div className="quality">
+              <span className="title">Calidad:</span>
+              <span className="data">{Product?.quality}</span>
+            </div>
+            <div className="code">
+              <span className="title">Código:</span>
+              <span className="data">{Product?.code}</span>
+            </div>
+          </section>
+        </div>
+      </section>
+
+      <section className="div2 carousel portion">
+        <swiper-container
+          slidesPerView={1}
+          autoplay={true}
+          pagination="true"
+          speed={1000}
+        >
+          {Product?.images.map((image, index) => (
+            <swiper-slide key={index}>
+              <img src={image} />
+            </swiper-slide>
+          ))}
+        </swiper-container>
+      </section>
+
+      <section className="div4 prices portion">
+        <span className="price">
+          {new Intl.NumberFormat("es-AR", {
+            style: "currency",
+            currency: "ARS",
+          }).format(Product?.price)}{" "}
+          X {Product?.priceUnit}
+        </span>
+        <span className="salesUnit">
+          <span>Unidad de venta:</span>
+          {Product?.salesUnit}
+        </span>
+      </section>
+
+      <section className="div5 actions portion">actions</section>
     </div>
   );
 };
