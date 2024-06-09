13f3800aabe4799a6fa8f1d084b684a947075988
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-06-09 20:34:52 -0300

    Advanced Product List

--- src/Components/ProductListFilters/ProductListFilters.jsx
+++ src/Components/ProductListFilters/ProductListFilters.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const ProductListFilters = () => {
   return (
-    <section className='ProductListFilters'>ProductListFilters</section>
+    <section className='ProductListFilters'>
+
+      <article className="FilterContainer">
+        <h3>Categoría</h3>
+      </article>
+
+      <article className="FilterContainer">
+        <h3>Proveedor</h3>
+      </article>
+
+      <article className="FilterContainer">
+        <h3>Medida</h3>
+      </article>
+
+      <article className="FilterContainer">
+        <h3>Precio</h3>
+      </article>
+
+      <article className="SingleFilterContainer">
+        <h3>Descuento</h3>
+      </article>
+
+    </section>
   )
 }
 
