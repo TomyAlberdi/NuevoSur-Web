ceffccdd6128a1563f54df241cb0b671012412ca
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2024-07-19 18:23:14 -0300

    Adapted Product Cards & Filters to backend

--- src/Components/TopProducts/TopProducts.jsx
+++ src/Components/TopProducts/TopProducts.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import data from "@/Utils/productList.json";
 import ProductCard from "@/Components/ProductCard/ProductCard";
 
 const TopProducts = () => {
+
+  const [Data, setData] = useState([])
+
+  useEffect(() => {
+    let getProductsUrl = `http://localhost:8080/product/list?size=8`;
+    fetch(getProductsUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.content);
+      })
+      .catch((err) => console.error(err));
+  })
+
   return (
     <div className="TopProducts">
       <h2>Productos Destacados</h2>
       <section className="productList">
-        {data.slice(0,8).map((product, index) => (
+        {Data?.map((product, index) => (
           <ProductCard key={index} data={product} />
         ))}
       </section>
